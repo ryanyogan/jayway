b882ef514d754370c762e2f94e97f1cc649429d5
Ryan Yogan <ryanyogan@gmail.com>
2017-01-15 14:40:34 -0800

    added in basic form validation

--- src/components/SaveProductForm.vue
+++ src/components/SaveProductForm.vue
@@ -1,6 +1,6 @@
 <template>
   <form>
-    <div class="form-group">
+    <div class="form-group" :class="[{ 'has-danger': formErrors.name }]">
       <label for="productName">Product name</label>
       <input
         type="text"
@@ -10,6 +10,12 @@
         maxlength="32"
         placeholder="Enter product name"
       />
+      <div
+        v-if="formErrors.name"
+        class="form-control-feedback"
+      >
+        {{formErrors.name}}
+      </div>
     </div>
     <div class="form-group">
       <label for="productDescription">Product description <small class="text-muted">(optional)</small></label>
@@ -22,7 +28,7 @@
         placeholder="Enter description"
       ></textarea>
     </div>
-    <div class="form-group">
+    <div class="form-group" :class="[{ 'has-danger': formErrors.price }]">
       <label for="price">Price</label>
       <input
         type="number"
@@ -32,6 +38,12 @@
         placeholder="Enter Price"
         number
       />
+      <div
+        v-if="formErrors.price"
+        class="form-control-feedback"
+      >
+        {{formErrors.price}}
+      </div>
     </div>
     <button type="submit" @click.prevent="onSubmit" class="btn btn-primary">
       {{ product.id ? 'Update' : 'Save' }} product
@@ -44,13 +56,48 @@
 <script>
 export default {
   props: ['product'],
+  data() {
+    return {
+      formErrors: {},
+    };
+  },
+  watch: {
+    product() {
+      this.formErrors = {};
+    },
+  },
   methods: {
+    validateForm() {
+      const errors = {};
+
+      if (!this.product.name) {
+        errors.name = 'Name is required.';
+      }
+
+      if (!this.product.price) {
+        errors.price = 'Price is required.';
+      }
+
+      this.formErrors = errors;
+
+      return Object.keys(errors).length === 0;
+    },
     onSubmit() {
-      this.$emit('submit', this.product);
+      if (this.validateForm()) {
+        this.$emit('submit', this.product);
+      }
     },
     onCancel() {
+      this.formErrors = {};
+
       this.$emit('cancel');
     },
   },
 };
 </script>
+
+<style scoped>
+form {
+  margin-bottom: 24px;
+}
+</style>
