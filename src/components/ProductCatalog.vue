d111cd6a82064878d3905669e73a2aa3db8029d6
Ryan Yogan <ryanyogan@gmail.com>
2017-01-15 14:56:51 -0800

    added in routing, header, logo, product catalog stub

---
+++ src/components/ProductCatalog.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>Product Catalog</p>
+</template>
+
+<script>
+export default {};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,31 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import App from './App';
+import ProductCatalog from './components/ProductCatalog';
+import ManageProducts from './components/ManageProducts';
 
 import './styles/style.scss';
 
 import '../node_modules/bootstrap/dist/js/bootstrap';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/home', alias: '/', component: ProductCatalog },
+  { path: '/manage-products', component: ManageProducts },
+];
+
+const router = new VueRouter({
+  routes,
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App },
+  router,
 });
